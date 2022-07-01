c6860ae9225833e768340d857f04cd923978c96e
Nicole <nicole@nicoleiliana.com>
2022-06-30 22:58:34 -0700

    Fixed first route, started logic for second

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from articleRecommender import inputStorage, aboutYou, yourGoals, jobHuntingStatus 
 ## from flask_restful import Api, Resource, reqparse
 ## from flask_cors import CORS #comment this on deployment
@@ -7,6 +7,28 @@ from articleRecommender import inputStorage, aboutYou, yourGoals, jobHuntingStat
 ## from Landing import Landing
 
 app = Flask(__name__)
+
+dict = {
+    1: { # About You
+        'isDesigner': 0,
+        'aspiringDesigner': 0
+        },
+    2: { # Your Goals
+        'improvingSkills': 0,
+        'findingNewJob': 0,
+        'breakingIn': 0
+        },
+    3: { # Job Hunting Status
+        'activelyApplying': 0,
+        'interviewNoStage': 0,
+        'interviewEarlyStage': 0,
+        'interviewMidStage': 0,
+        'interviewLateStage': 0,
+        }
+    }
+
+def storeValue(i, key):
+    dict[i][key] += 1
 
 ## @app.before_request()
 ## create a function to capture the user's last visited page
@@ -18,26 +40,55 @@ def entry_page() -> 'html':
 
 @app.route('/test')
 def hello() -> str:
-    return 'Hello world from Flask'
+    return dict
 
 @app.route('/current-designer', methods=['GET', 'POST'])
 def currentdesigner_page() -> 'html':
     return render_template('your-role.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add() -> 'html':
     if request.method == 'POST':
-        isDesignerInput = request.form.get['currentdesigner']
-        if isDesignerInput == 'yes':
-            return redirect('/your-goals')
+        if request.form.get('currentdesigner') == 'yes':
+            storeValue(1, 'isDesigner')
         else:
+            storeValue(1, 'aspiringDesigner')
             return redirect('/aspiring-designer')
+    return redirect('/your-goals')
 
-
-@app.route('/aspiring-designer')
+@app.route('/aspiring-designer', methods=['GET', 'POST'])
 def aspiringdesigner_page() -> 'html':
     return render_template('aspiring-designer.html')
 
-@app.route('/your-goals')
+@app.route('/your-goals', methods=['GET', 'POST'])
 def yourgoals_page() -> 'html':
     return render_template('your-goals.html')
+
+@app.route('/add2', methods=['GET', 'POST'])
+def add2() -> 'html':
+    if request.method == 'POST':
+        if request.form.get('goal') == 'skills':
+            storeValue(1, 'improvingSkills')
+        if request.form.get('goal') == 'newjob':
+            storeValue(1, 'findingNewJob')
+            return redirect('/interview-status')
+        if request.form.get('goal') == 'tech':
+            storeValue(1, 'breakingIn')
+            return redirect('/interview-status')
+        else:
+            if request.form.get('aspiringdesigner') == 'yes':
+                storeValue(1, 'aspiringDesigner')
+                return redirect('/your-goals')
+    return redirect('/results2')
+
+
+    if yourGoalsValue == 'A':
+        dict[2]['improvingSkills'] += 1
+    else:
+        if yourGoalsValue == 'B':
+            dict[2]['findingNewJob'] += 1
+        else:
+            dict[2]['breakingIn'] += 1
 
 
 @app.route('/interview-status')
@@ -64,7 +115,7 @@ def results_page() -> 'html':
 # def entry_page() -> 'html':
   #  return render_template('entry.html', the_title='welcome to the app recommender')
 
-app.run()
+app.run(debug=True)
 
 
 #### Old code ####
